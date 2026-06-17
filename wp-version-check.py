13026f74df068affb1e8581feec90aecdc03ba7e
Andrew Martin <netranger+gh3@wearehackerone.com>
2026-06-17 16:50:28 -0600

    Update wp-version-check.py

--- wp-version-check.py
+++ wp-version-check.py
@@ -1,1 +1,16 @@
+import sys, requests
 
+
+def parse_version(text):
+  # TODO implement
+  return "7.5.0"
+
+print("Usage: wp-version-check.py <url to scan>")
+
+url = sys.argv[1]
+
+r = requests.get(url)
+
+version = parse_version(r.text)
+
+print("[ ] Site appears to be running WordPress " + version)
